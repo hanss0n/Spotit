a5f4bf834914cf4180191811cbbd81d657842e31
bjhans <bjhans@kth.se>
2020-07-24 20:14:18 +0200

    Set up basic authentication for testing

--- app/app.py
+++ app/app.py
@@ -1,20 +1,11 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import os
 
-if os.getenv('SPOTIPY_CLIENT_ID') is not None:
-    client_id = os.getenv('SPOTIPY_CLIENT_ID')
-else:
-    print('Enter client id:')
-    client_id = str(input())
-    os.environ['SPOTIPY_CLIENT_ID'] = client_id
+print('Enter client id:')
+client_id = str(input())
 
-if os.getenv('SPOTIPY_CLIENT_SECRET') is not None:
-    client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
-else:
-    print('Enter client secret:')
-    client_secret = str(input())
-    os.environ['SPOTIPY_CLIENT_SECRET'] = client_secret
+print('Enter client secret:')
+client_secret = str(input())
 
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)

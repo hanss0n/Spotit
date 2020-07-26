db49b3c2a532464105e2f9befbf057eb813a9457
bjhans <bjhans@kth.se>
2020-07-27 00:35:43 +0200

    * Added some initial spotify services * Now loads environment variables on launch * Set up initial HTTP endpoints * Minor refactoring

--- app/controller/song_list_controller.py
+++ app/controller/song_list_controller.py
@@ -3,16 +3,19 @@ from flask import Blueprint, request
 song_list = Blueprint('song_list', __name__)
 
 
+# TODO: implement
 @song_list.route("/spotify_list/edges", methods=["GET"])
 def edges():
     return "edges"
 
 
+# TODO: implement
 @song_list.route("/spotify_list/vertices", methods=["GET"])
 def vertices():
     return "vertices"
 
 
+# TODO: implement
 @song_list.route("/spotify_list/<list_uri>", methods=["POST"])
 def spotify_list_uri(list_uri):
     return list_uri

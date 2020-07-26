9479842a90cb941f57e20fa7ea385ddd0c937bc0
bjhans <bjhans@kth.se>
2020-07-27 00:24:09 +0200

    * Added some initial spotify services * Now loads environment variables on launch * Set up initial HTTP endpoints * Minor refactoring

--- app/controller/auth_controller.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-
-
-# TODO: fix this garb
-@auth.route("/auth", methods=['GET'])
-def kek():
-    return "you are authenticated"
--- app/run.py
+++ app/run.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from app.controller.song_list_controller import song_list
-from app.controller.auth_controller import auth
 
 app = Flask(__name__)
 app.register_blueprint(song_list)
-app.register_blueprint(auth)
 
 if __name__ == '__main__':
     app.run()

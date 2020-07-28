d79e98e4b1828f2f57147e549c9110062a227df3
felixlut <felixlut@gmail.com>
2020-07-28 14:28:10 +0200

    Resctructure and break out filtering/scaling into smaller functions
    There has been some additional functions added as to make the
    program more understandable. One difference worth noting is the
    fact that the features are now stored in a dictionary by id, i.e.
    {id: {duration: 25000, ...}}, as opposed to the way it was before.

--- app/service/spotify_service.py
+++ app/service/spotify_service.py
@@ -15,34 +15,82 @@ def get_playlist_id_list(playlist_uri):
     return [track['track']['id'] for track in tracks]
 
 
-def get_scalar_song_features(song_id):
-    # Fetch all of the features of the track
-    features = spotify.audio_features(song_id)[0]
+def preprocess_features(track_ids):
+    # Fetch the audio features of each track
+    features = spotify.audio_features(track_ids)
 
-    # Filter out the non-number attributes
-    filtered_features = {attribute: value for (attribute, value) in features.items() if isinstance(value, numbers.Number)}
+    # Store by id for simplicitys sake
+    features_by_track_id = {features[i]['id']: features[i] for i in range(len(features))}
 
-    # And discard the duration attribute
-    del filtered_features['duration_ms']
+    # Filter out unwanted features
+    filtered_features_by_id = filter_features(features_by_track_id)
+
+    # Find out the range of the time_signature attribute for the playlist
+    min, max = get_time_signature_range(filtered_features_by_id)
 
     # Rescale the Key, Loudness, Tempo and time_signature features to [0, 1]
-    # Key is denoted by standard Pitch Class notation, where -1 is used if no key is detected
-    # We alter the current notation, [-1, 11] --> [0, 1]
-    filtered_features['key'] += 1
-    filtered_features['key'] /= 12
+    scaled_features = rescale_features(filtered_features_by_id, min, max)
 
-    # Loudness is previously measured in decibel, in range [-60, 0]. We want [0, 1]
-    filtered_features['loudness'] += 60
-    filtered_features['loudness'] /= 60
-
-    # Tempo is measured in BPM. Spotify seems to measure up to 250 BPM, which is why this values is used for scaling
-    filtered_features['tempo'] /= 250
-
-    # TODO: Time_signature
-    del filtered_features['time_signature']
-
-    return filtered_features
+    return scaled_features
 
 
+
+def filter_features(features_by_id):
+    for track_features in features_by_id.items():
+
+        # Filter out the non-number attributes
+        filtered_features = {feature: value for (feature, value) in track_features[1].items() if isinstance(value, numbers.Number)}
+
+        # And discard the duration attribute
+        del filtered_features['duration_ms']
+
+        # Replace the non-filtered attributes
+        features_by_id[track_features[0]] = filtered_features
+
+    return features_by_id
+
+
+def get_time_signature_range(features_by_id):
+    first_val = features_by_id[list(features_by_id.keys())[0]]['time_signature']
+    min, max = first_val, first_val
+    for track_features in features_by_id.items():
+        time_sig = track_features[1]['time_signature']
+        if time_sig > max:
+            max = time_sig
+        if time_sig < min:
+            min = time_sig
+
+    return min, max
+
+
+def rescale_features(features_by_id, time_sig_min, time_sig_max):
+    for track_features in features_by_id.items():
+
+        rescaled_features = track_features[1]
+
+        print(rescaled_features)
+
+        # Rescale the Key, Loudness, Tempo and time_signature features to [0, 1]
+        # Key is denoted by standard Pitch Class notation, where -1 is used if no key is detected
+        # We alter the current notation, [-1, 11] --> [0, 1]
+        rescaled_features['key'] += 1
+        rescaled_features['key'] /= 12
+
+        # Loudness is previously measured in decibel, in range [-60, 0]. We want [0, 1]
+        rescaled_features['loudness'] += 60
+        rescaled_features['loudness'] /= 60
+
+        # Tempo is measured in BPM. Spotify seems to measure up to 250 BPM, which is why this values is used for scaling
+        rescaled_features['tempo'] /= 250
+
+        # TODO: Time_signature
+        del rescaled_features['time_signature']
+
+        # Save the update values
+        features_by_id[track_features[0]] = rescaled_features
+
+        print(features_by_id)
+
+    return features_by_id
 
 

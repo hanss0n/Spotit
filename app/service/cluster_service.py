71e9d1e24fe73131674c74e09a3ac1484e9061d1
bjhans <bjhans@kth.se>
2020-07-31 16:16:04 +0200

    More refactoring

--- app/service/cluster_service.py
+++ app/service/cluster_service.py
@@ -1,5 +1,4 @@
-# TODO: get rid of all spotify service code from here
-from sklearn.cluster import KMeans, MiniBatchKMeans
+from sklearn.cluster import MiniBatchKMeans
 from sklearn.preprocessing import StandardScaler
 
 import app.service.spotify_service as ss
@@ -7,54 +6,42 @@ import pandas as pd
 import numpy as np
 
 
-def track_to_arr(feature_dict):
-    identifier = feature_dict[0]
-    danceability = feature_dict[1]['danceability']
-    energy = feature_dict[1]['energy']
-    speechiness = feature_dict[1]['speechiness']
-    acousticness = feature_dict[1]['acousticness']
-    instrumentalness = feature_dict[1]['instrumentalness']
-
-    return [identifier, danceability, energy, speechiness, acousticness, instrumentalness]
+def __track_to_arr(feature_dict, features):
+    song_id = feature_dict[0]
+    track = [song_id]
+    for i in range(len(features)):
+        track.append(feature_dict[1][features[i]])
+    return track
 
 
-def to_data_frame(song_features):
+def __to_data_frame(song_list, cluster_features):
     tracks = []
-    song_list = ss.get_playlist_id_list('spotify:playlist:68cbyzto5Sm9aoMgHRatb6')
-    song_list = ss.preprocess_features(song_list)
-
     for song in song_list.items():
-        tracks.append(track_to_arr(song))
+        tracks.append(__track_to_arr(song, cluster_features))
 
-    song_data = pd.DataFrame(tracks, columns=['id', 'danceability', 'energy', 'speechiness',
-                                              'acousticness', 'instrumentalness'])
+    song_data = pd.DataFrame(tracks, columns=['id'] + cluster_features)
+    df_labels = song_data['id']
+    df_songs = song_data[cluster_features]
 
-    features = ['danceability', 'energy', 'speechiness',
-                'acousticness', 'instrumentalness']
+    return df_songs, df_labels
 
-    labels = song_data['id']
 
-    df_cluster = song_data[features]
-    cluster = np.array(df_cluster)
+def __cluster_data_frame(df_songs, df_labels, num_cluster):
+    cluster = np.array(df_songs)
     scaler = StandardScaler()
     scaler.fit(cluster)
     cluster = scaler.transform(cluster)
-    k = 5
 
-    km = MiniBatchKMeans(n_clusters=k, init='k-means++', random_state=111)
-    km = km.fit(cluster).labels_
+    kmeans = MiniBatchKMeans(n_clusters=num_cluster, init='k-means++', random_state=111)
+    kmeans = kmeans.fit(cluster).labels_
+    cluster_dict = {cluster_label: [] for cluster_label in range(num_cluster)}
 
-    cluster_dict = {cluster_label: [] for cluster_label in range(k)}
-
-    for song_id, cluster_label in enumerate(km):
-        cluster_dict[cluster_label].append(labels[song_id])
+    for song_id, cluster_label in enumerate(kmeans):
+        cluster_dict[cluster_label].append(df_labels[song_id])
 
     return cluster_dict
 
 
-def cluster_songs(num_clusters, song_features):
-    print("lol")
-
-
-if __name__ == '__main__':
-    print(to_data_frame("lololol"))
+def cluster(song_list, features, num_clusters):
+    df_songs, df_labels = __to_data_frame(song_list, features)
+    return __cluster_data_frame(df_songs, df_labels, num_clusters)

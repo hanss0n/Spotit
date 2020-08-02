cfb9cd1677543fb782652a4359498153ef840ba4
felixlut <felixlut@gmail.com>
2020-08-02 18:17:17 +0200

    WIP: Implement convertion of cluster to JSON

--- app/service/song_list_service.py
+++ app/service/song_list_service.py
@@ -1,5 +1,6 @@
 from app.service.cluster_service import cluster
 from app.service.spotify_service import get_playlist_id_list, fetch_features
+import json
 
 
 def get_cluster_by_list_id(list_id):
@@ -8,4 +9,13 @@ def get_cluster_by_list_id(list_id):
     features = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'speechiness']
     return cluster(song_features, features, 5)
 
+
+def convert_cluster_to_json(cluster):
+    tracks = {'track_list': []}
+    for track in cluster:
+        track_info = fetch_track(track['song_id'])
+        track_info['cluster_id'] = track['cluster_id']
+        tracks['track_list'].append(track_info)
+
+    return json.dumps(tracks)
 

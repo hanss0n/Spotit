2438b54296c483f1b889f2520d829863ab65fa98
felixlut <felixlut@gmail.com>
2020-08-02 22:22:33 +0200

    Alter song_lust_controller to adhere to the structure of the service

--- app/controller/song_list_controller.py
+++ app/controller/song_list_controller.py
@@ -7,4 +7,4 @@ song_list = Blueprint('song_list', __name__)
 
 @song_list.route("/spotify_list/<list_uri>", methods=["POST"])
 def cluster_from_uri(list_uri):
-    return json.dumps(song_list_service.get_cluster_by_list_id(list_uri), indent=4)
+    return get_json_cluster_by_list_id(list_uri)

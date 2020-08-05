7bf3589abd0c9cfa84245b6b34a8e5790b551ac6
Felix <felix.luthman@gmail.com>
2020-08-05 23:43:10 +0200

    Change the structure of the song_list_service file
    The functionality of this code should be the same, but now it
    circumvents an unnecessary API call to Spotify by fetching the
    same data as this call would have in a previous call instead
    
    Issue: #25

--- app/service/song_list_service.py
+++ app/service/song_list_service.py
@@ -1,27 +1,28 @@
 from app.service.cluster_service import cluster
-from app.service.spotify_service import get_playlist_id_list, fetch_features, fetch_track
+from app.service.spotify_service import get_tracks_by_list_id, fetch_features, fetch_track
 import json
 
 
-def get_cluster_by_list_id(list_id, num_of_clusters=5):
+def get_cluster_by_track_ids(track_ids, num_of_clusters=5):
     features_to_consider = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'speechiness']
-    song_ids = get_playlist_id_list(list_id)
-    song_features = fetch_features(song_ids, features_to_consider)
+    song_features = fetch_features(track_ids, features_to_consider)
     return cluster(song_features, features_to_consider, num_of_clusters)
 
 
-def get_json_cluster_by_list_id(list_id):
-    cluster = get_cluster_by_list_id(list_id)
-    json_object = convert_cluster_to_json(cluster)
-    return json_object
+def get_json_track_list_by_list_id(list_id):
+    # Get a list of tracks
+    track_list = get_tracks_by_list_id(list_id)
 
+    # Extract the ids of the tracks
+    track_ids = [track['track']['id'] for track in track_list]
 
-def convert_cluster_to_json(cluster):
-    tracks = {'track_list': []}
-    for track in cluster.items():
-        track_info = fetch_track(track[0])
-        track_info['cluster_id'] = int(track[1])
-        tracks['track_list'].append(track_info)
+    # Create a cluster from the tracks
+    cluster = get_cluster_by_track_ids(track_ids)
 
-    return json.dumps(tracks, indent=4)
+    # Append the cluster_id to the track_list
+    for track in track_list:
+        # Add the cluster_id (as a normal int, as opposed to int32)
+        track['track']['cluster_id'] = int(cluster[track['track']['id']])
 
+    # Convert the cluster into json format
+    return json.dumps(track_list, indent=4)

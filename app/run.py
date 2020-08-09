da9ae35d940f9ed898230c52a60d43011d61ba9e
felixlut <34520175+felixlut@users.noreply.github.com>
2020-08-09 16:09:33 +0200

    Implement CORS functionality (#28)

--- app/run.py
+++ app/run.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from app.controller.song_list_controller import song_list
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(song_list)
 
 if __name__ == '__main__':

ba02b1a7849ef6bc11305d0e3991b766220914e5
Björn Hansson <bjhans@kth.se>
2020-08-10 17:21:26 +0200

    now takes a json object with attributes (#30)

--- app/service/song_list_service.py
+++ app/service/song_list_service.py
@@ -3,13 +3,16 @@ from app.service.spotify_service import get_tracks_by_list_id, fetch_features, f
 import json
 
 
-def get_cluster_by_track_ids(track_ids, num_of_clusters=5):
-    features_to_consider = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'speechiness']
+def get_cluster_by_params(track_ids, num_of_clusters, features_to_consider):
     song_features = fetch_features(track_ids, features_to_consider)
     return cluster(song_features, features_to_consider, num_of_clusters)
 
 
-def get_json_track_list_by_list_id(list_id):
+def get_cluster_list_json(cluster_params):
+    list_id = cluster_params["list_uri"]
+    features_to_consider = cluster_params["attributes"]
+    num_clusters = int(cluster_params["num_clusters"])
+
     # Get a list of tracks
     track_list = get_tracks_by_list_id(list_id)
 
@@ -17,12 +20,12 @@ def get_json_track_list_by_list_id(list_id):
     track_ids = [track['track']['id'] for track in track_list]
 
     # Create a cluster from the tracks
-    cluster = get_cluster_by_track_ids(track_ids)
+    feature_cluster = get_cluster_by_params(track_ids, num_clusters, features_to_consider)
 
     # Append the cluster_id to the track_list
     for track in track_list:
         # Add the cluster_id (as a normal int, as opposed to int32)
-        track['track']['cluster_id'] = int(cluster[track['track']['id']])
+        track['track']['cluster_id'] = int(feature_cluster[track['track']['id']])
 
     # Convert the cluster into json format
     return json.dumps(track_list, indent=4)

901c47c152add064c598f4e5201a47acda37f5aa
bjhans <bjhans@kth.se>
2020-08-13 15:51:15 +0200

    Merged frontend into backend Added docker file template

---
+++ web/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="app">
+        <Header/>
+        <GenerateCluster v-on:generate-cluster="getSongData"/>
+        <ClusterGraph v-bind:song_data="song_data" v-bind:num_clusters="num_clusters"/>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    import Header from '@/components/layout/Header';
+    import GenerateCluster from "@/components/GenerateCluster";
+    import ClusterGraph from "@/components/ClusterGraph";
+
+    export default {
+        name: 'App',
+        components: {
+            ClusterGraph,
+            GenerateCluster,
+            Header
+        },
+        data() {
+            return {
+                num_clusters: 5,
+                song_data: []
+            }
+        },
+        methods: {
+            getSongData(params) {
+                this.num_clusters = params["num_clusters"]
+                axios.post('http://127.0.0.1:5000/spotify_list/', params)
+                    .then(x => this.song_data = x.data)
+                    .catch(e => console.log(e))
+            }
+        }
+    }
+</script>
+
+<style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        line-height: 1.4;
+        background: black;
+    }
+
+    .btn {
+        display: inline-block;
+        border: none;
+        background: #555;
+        color: #fff;
+        padding: 7px 20px;
+        cursor: pointer;
+    }
+
+    .btn:hover {
+        background: #666;
+    }
+</style>
---
+++ web/src/components/layout/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="header">
+        <h1>Clustrify</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+    .header {
+        background: #292929;
+        color: #fff;
+        text-align: center;
+        padding: 5px;
+    }
+
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+        text-decoration: none;
+    }
+</style>
